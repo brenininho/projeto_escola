e188341782619b2254ba1128e19cac787e24a2e2
Joao Roma <joaoricardoroma@gmail.com>
2021-03-06 15:41:35 -0300

    reorganizei os professores criei o criar e editar os professores e arrumei o app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+conn = sqlite3.connect("system.db")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS professor ( name text, school_year text)")
+conn.close()
 
 @app.route('/')
 def home():
@@ -14,7 +18,7 @@ def aluno():
     conn = sqlite3.connect("system.db")
     c = conn.cursor()
     # c.execute("CREATE TABLE IF NOT EXISTS student (name text, school_year text)")
-    c.execute("INSERT INTO student VALUES ('Breno Valle', 3º Ano Médio'")
+    c.execute("INSERT INTO student VALUES ('Breno Valle', 3 Ano Médio'")
     conn.commit()
     data = c.fetchall()
     return render_template("aluno.html", data=data)
@@ -24,11 +28,28 @@ def aluno():
 def professor():
     conn = sqlite3.connect("system.db")
     c = conn.cursor()
-    # c.execute("CREATE TABLE IF NOT EXISTS professor (name text, school_year text)")
-    conn.commit()
-    return render_template("professor.html")
+    c.execute("SELECT rowid, * FROM professor")
+    data = c.fetchall()
+    return render_template("professor.html", data=data)
 
 
 @app.route('/nota')
 def nota():
     return render_template("nota.html")
+
+
+@app.route('/professor/criar')
+def professor_criar():
+    return render_template("professor_criar.html")
+
+
+@app.route('/professor/deletar/<int:id>')
+def professor_deletar(id):
+    pass
+
+
+
+@app.route('/professor/editar/<int:id>')
+def professor_editar(id):
+
+    return render_template("professor_editar.html")
